6c3c377964aa7f17c2b1992bda20f0652229cf62
felix <779358334@qq.com>
2020-06-22 15:51:58 +0800

    产品注册模板类+单例工厂模板类

---
+++ Src/product_register_template_and_singleton_factory_template.cpp
@@ -0,0 +1,1 @@
+#include "product_register_template_and_singleton_factory_template.h"
--- main.cpp
+++ main.cpp
@@ -1,32 +1,38 @@
 #include <iostream>
-#include "Src/abstract_template_factory.h"
+#include "Src/product_register_template_and_singleton_factory_template.h"
 
 using namespace std;
 
 int main(int argc, char **argv)
 {
     std::cout << "设计模式测试用例" << std::endl;
-    
-    // 构造耐克鞋的工厂对象
-    FjhApp::ConcreteFactory<FjhApp::Shoes, FjhApp::NiKeShoes> nikeFactory;
-    // 创建耐克鞋对象
-    FjhApp::Shoes *pNiKeShoes = nikeFactory.CreateProduct();
-    // 打印耐克鞋广告语
-    pNiKeShoes->Show();
 
-    // 构造优衣库衣服的工厂对象
-    FjhApp::ConcreteFactory<FjhApp::Clothe, FjhApp::UniqloClothe> uniqloFactory;
-    // 创建优衣库衣服对象
-    FjhApp::Clothe *pUniqloClothe = uniqloFactory.CreateProduct();
-    // 打印优衣库广告语
-    pUniqloClothe->Show();
 
-    // 释放资源
-    delete pNiKeShoes;
-    pNiKeShoes = NULL;
+    // ========================== 生产耐克球鞋过程 ===========================//
+   // 注册产品种类为Shoes（基类），产品为NiKe（子类）到工厂，产品名为nike
+   FjhApp::ProductRegistrar<FjhApp::Shoes, FjhApp::NiKeShoes> nikeShoes("nike");
+   // 从工厂获取产品种类为Shoes，名称为nike的产品对象
+   FjhApp::Shoes *pNiKeShoes = FjhApp::ProductFactory<FjhApp::Shoes>::Instance().GetProduct("nike");
+   // 显示产品的广告语
+   pNiKeShoes->Show();
+   // 释放资源
+   if (pNiKeShoes)
+   {
+      delete pNiKeShoes;
+   }
 
-    delete pUniqloClothe;
-    pUniqloClothe = NULL;
+   // ========================== 生产优衣库衣服过程 ===========================//
+   // 注册产品种类为Clothe（基类），产品为UniqloClothe（子类）到工厂，产品名为uniqlo
+   FjhApp::ProductRegistrar<FjhApp::Clothe, FjhApp::UniqloClothe> adidasShoes("uniqlo");
+   // 从工厂获取产品种类为Shoes，名称为adidas的产品对象
+   FjhApp::Clothe *pUniqloClothe = FjhApp::ProductFactory<FjhApp::Clothe>::Instance().GetProduct("uniqlo");
+   // 显示产品的广告语
+   pUniqloClothe->Show();
+   // 释放资源
+   if (pUniqloClothe)
+   {
+      delete pUniqloClothe;
+   }
 
     return 0;
 }
